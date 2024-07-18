1493718a25ec9676d45760a7907d441038e71568
Alexprinse <princebadampudi@gmail.com>
2024-07-18 13:21:17 +0530

    labs update

--- src/Components/SubjectList.js
+++ src/Components/SubjectList.js
@@ -4,47 +4,146 @@ import './styles.css';
 
 const subjects = {
   CSE: {
-    '1-1': ['Calculus and Linear Algebra', 'Basic Electrical and Electronics Engineering', 'Problem Solving and Programming Through C ', 'Engineering Graphics & Computer Drafting ','English-Language communication Skills Lab-I','Indian Constitution'],
-    '1-2': ['Data Structures', 'Discrete Mathematics', 'Engineering Physics', 'Managerial Economics and Finance Analysis','Object Oriented Programming through Java','Environmental Science'],
-    '2-1': ['Probability and Statistics','Digital Logic Design','Design & Analysis of Algorithms','Database Management Systems','Formal Languages & Automata Theory'],
-    '2-2':['Introduction to Operation Research','Computer Organization & Architecture ','Data Science with Python','Web Technologies','Compiler Design'],
-    '3-1':['Operating System','Computer Networks ','Software Engineering ','Mathematical Foundations for Data Science ','English-Language communication Skills Lab- II','Data Mining'],
-    '3-2': ['Cryptography and Networks Security','Artificial Intelligence','English-Language communication Skills Lab-I -III'],
-    '4-1':['Machine Learning']
-  },
-  CE:{
-    '1-1':['Engineering Chemistry','Differential Equations and Multivariable Calculus','Programming and Data Structures','Engineering Graphics and Computer Drafting','Aptitude and Reasoning'],
-    '1-2':['Engineering Physics','Mathematical Methods','Basic Electrical and Electronics Engineering','Engineering Mechanics','Engineering Geology ','Environmental Science'],
-    '2-1':['Management Economics and Financial Analysis','Building Materials and Construction','Concrete Technology','Mechanics of Fluids','Mechanics of Materials-I','Indian Constitution','Surveying-I'],
-    '2-2':['Hydraulics Engineering ','Mechanics of Materials-II','Soil Mechanics','Structural Analysis','Surveying-II ','Water Resources Engineering'],
-    '3-1':['Advanced Structural Analysis','Design of Reinforced concrete Structures','Environmental Engineering-I','Estimation and Costing','Transportation Engineering-I','English Language Communication Skills Lab-II'],
-    '3-2':['Design of Steel Structures','Environmental Engineering-II','Foundation Engineering','Transportation Engineering-II','English Language Communication Skills Lab-1']
-  },
-  EEE:{
-    '1-1':['Differential Equations and Multivariable calculus','Engineering Physics','Engineering Graphics & Computer Drafting','Electrical Technology','Introduction to Latest Technical Advancements','Programming & Data Structures '],
-    '1-2':['Mathematical Methods','Digital Logic Design ','Electronics Devices and Circuits','Network Theory','Introduction to AI/ML '],
-    '2-1':['Probability & Random Variables','Analog Electronic Circuits','Object Oriented Programming','Signals & Systems', 'Electrical Machines'],
-    '2-2':['Power Systems-I','Machine Learning','Control Systems ','Linear Integrated Circuits ','Power Electronics '],
-    '3-1':['Digital Signal Processing','Power Systems-II ','Electrical Vehicles','Embedded Systems','Product Design & Innovation','English Language communication skills Lab-2'],
-    '3-2':['English Language Communication skills lab-3 ','Indian Constitution'],
-    '4-1':['Environmental Science']
-  },
-  ECE:{
-    '1-1':['Differential Equations and Multivariable calculus','Engineering Physics','Signals and Systems','Introduction to Latest Technical Advancements','Programming & Data Structures'],
-    '1-2':['Mathematical Methods','Object Oriented Programming','Electronic Devices and Circuits','Network Theory','Engineering Graphics and Design'],
-    '2-1':['Probability & Random Variables','Analog Electronic Circuits','Digital Logic Design','Digital Signal Processing','Control Systems'],
-    '2-2':['Communication Systems-1','Digital System Design','Linear Integrated Circuits','Electromagnetic Waves & Guided Media'],
-    '3-1':['Computer Networks','Computer Organization & Architecture','Communication Systems- 2','English-Language Communication skills Lab-2'],
-    '3-2':['Product Design & Innovation','English-Language Communication skills Lab-3'],
-    '4-1':['Environmental Science']
+    '1-1': {
+      theory: ['Calculus and Linear Algebra', 'Basic Electrical and Electronics Engineering', 'Problem Solving and Programming Through C', 'Engineering Graphics & Computer Drafting', 'Indian Constitution'],
+      labs: ['English-Language Communication Skills Lab-I','BEEE Lab','PSPC Lab']
     },
-  ME:{
-    '1-1':['Differential Equations and Multivariable Calculus','English Language Communication Skills Lab-I','Engineering Physics','Basic Electrical and Electronics Engineering','Engineering Chemistry'],
-    '1-2':['Mathematical Methods','Engineering Mechanics','Material Science & Metallurgy','Programming and Data Structures','Engineering Graphics and Computer Drafting ','Environmental Science'],
-    '2-1':['Transform Calculus ','Kinematics of Machinery ','Thermodynamics','Mechanics of Solids','Manufacturing Processes'],
-    '2-2':['Design of Machine Elements','Dynamics of Machinery ','Fluid Mechanics & Hydraulic Machinery','Metal Cutting and Machine Tools','Probability and Statistics','Indian Constitution'],
-    '3-1':['Heat Transfer','Design of Transmission Elements','Applied Thermodynamics','Metrology and Mechanical Measurements','English Language Communication Skills Lab-II'],
-    '3-2':['Operations Research','Finite Element Method','Managerial Economics and Financial Analysis','English Language Communication Skills Lab-III']
+    '1-2': {
+      theory: ['Data Structures', 'Discrete Mathematics', 'Engineering Physics', 'Managerial Economics and Finance Analysis', 'Object Oriented Programming through Java', 'Environmental Science'],
+      labs: ['Engineering Physics Lab','OOPS Lab','Data Structures Lab']
+    },
+    '2-1': {
+      theory: ['Probability and Statistics', 'Digital Logic Design', 'Design & Analysis of Algorithms', 'Database Management Systems', 'Formal Languages & Automata Theory'],
+      labs: ['Design & Analysis of Algorithms Lab','Digital Logic Design Lab','Database Management Systems Lab']
+    },
+    '2-2': {
+      theory: ['Introduction to Operation Research', 'Computer Organization & Architecture', 'Data Science with Python', 'Web Technologies', 'Compiler Design'],
+      labs: ['Computer Organization & Architecture Lab','Data Science with Python Lab','Web Technologies Lab']
+    },
+    '3-1': {
+      theory: ['Operating System', 'Computer Networks', 'Software Engineering', 'Mathematical Foundations for Data Science', 'Data Mining'],
+      labs: ['Operating System Lab','Computer Networks Lab','Software Engineering Lab','English-Language Communication Skills Lab-II']
+    },
+    '3-2': {
+      theory: ['Cryptography and Networks Security', 'Artificial Intelligence'],
+      labs: ['English-Language Communication Skills Lab-III']
+    },
+    '4-1': {
+      theory: ['Machine Learning'],
+      labs: []
+    }
+  },
+  CE: {
+    '1-1': {
+      theory: ['Engineering Chemistry', 'Differential Equations and Multivariable Calculus', 'Programming and Data Structures', 'Engineering Graphics and Computer Drafting', 'Aptitude and Reasoning'],
+      labs: ['Engineering Chemistry Lab','English Language Communication Skills Lab-I','Programming and Data Structures Lab']
+    },
+    '1-2': {
+      theory: ['Engineering Physics', 'Mathematical Methods', 'Basic Electrical and Electronics Engineering', 'Engineering Mechanics', 'Engineering Geology', 'Environmental Science'],
+      labs: ['Engineering Physics Lab','Workshop']
+    },
+    '2-1': {
+      theory: ['Management Economics and Financial Analysis', 'Building Materials and Construction', 'Concrete Technology', 'Mechanics of Fluids', 'Mechanics of Materials-I', 'Indian Constitution', 'Surveying-I'],
+      labs: ['Mechanics of Materials Lab','Surveying Lab']
+    },
+    '2-2': {
+      theory: ['Hydraulics Engineering', 'Mechanics of Materials-II', 'Soil Mechanics', 'Structural Analysis', 'Surveying-II', 'Water Resources Engineering'],
+      labs: ['Concrete Technology Lab','Hydraulics Engineering Lab']
+    },
+    '3-1': {
+      theory: ['Advanced Structural Analysis', 'Design of Reinforced concrete Structures', 'Environmental Engineering-I', 'Estimation and Costing', 'Transportation Engineering-I'],
+      labs: ['English Language Communication Skills Lab-II','Soil Mechanics Lab','Transportation Engineering Lab']
+    },
+    '3-2': {
+      theory: ['Design of Steel Structures', 'Environmental Engineering-II', 'Foundation Engineering', 'Transportation Engineering-II'],
+      labs: ['English Language Communication Skills Lab-III','Environmental Engineering Lab']
+    }
+  },
+  EEE: {
+    '1-1': {
+      theory: ['Differential Equations and Multivariable calculus', 'Engineering Physics', 'Engineering Graphics & Computer Drafting', 'Electrical Technology', 'Introduction to Latest Technical Advancements', 'Programming & Data Structures'],
+      labs: []
+    },
+    '1-2': {
+      theory: ['Mathematical Methods', 'Digital Logic Design', 'Electronics Devices and Circuits', 'Network Theory', 'Introduction to AI/ML'],
+      labs: []
+    },
+    '2-1': {
+      theory: ['Probability & Random Variables', 'Analog Electronic Circuits', 'Object Oriented Programming', 'Signals & Systems', 'Electrical Machines'],
+      labs: []
+    },
+    '2-2': {
+      theory: ['Power Systems-I', 'Machine Learning', 'Control Systems', 'Linear Integrated Circuits', 'Power Electronics'],
+      labs: []
+    },
+    '3-1': {
+      theory: ['Digital Signal Processing', 'Power Systems-II', 'Electrical Vehicles', 'Embedded Systems', 'Product Design & Innovation'],
+      labs: ['English Language communication skills Lab-II']
+    },
+    '3-2': {
+      theory: [],
+      labs: ['English Language Communication skills lab-III', 'Indian Constitution']
+    },
+    '4-1': {
+      theory: ['Environmental Science'],
+      labs: []
+    }
+  },
+  ECE: {
+    '1-1': {
+      theory: ['Differential Equations and Multivariable calculus', 'Engineering Physics', 'Signals and Systems', 'Introduction to Latest Technical Advancements', 'Programming & Data Structures'],
+      labs: []
+    },
+    '1-2': {
+      theory: ['Mathematical Methods', 'Object Oriented Programming', 'Electronic Devices and Circuits', 'Network Theory', 'Engineering Graphics and Design'],
+      labs: []
+    },
+    '2-1': {
+      theory: ['Probability & Random Variables', 'Analog Electronic Circuits', 'Digital Logic Design', 'Digital Signal Processing', 'Control Systems'],
+      labs: []
+    },
+    '2-2': {
+      theory: ['Communication Systems-1', 'Digital System Design', 'Linear Integrated Circuits', 'Electromagnetic Waves & Guided Media'],
+      labs: []
+    },
+    '3-1': {
+      theory: ['Computer Networks', 'Computer Organization & Architecture', 'Communication Systems- 2'],
+      labs: ['English-Language Communication skills Lab-II']
+    },
+    '3-2': {
+      theory: ['Product Design & Innovation'],
+      labs: ['English-Language Communication skills Lab-III']
+    },
+    '4-1': {
+      theory: ['Environmental Science'],
+      labs: []
+    }
+  },
+  ME: {
+    '1-1': {
+      theory: ['Differential Equations and Multivariable Calculus', 'Engineering Physics', 'Basic Electrical and Electronics Engineering', 'Engineering Chemistry'],
+      labs: ['English Language Communication Skills Lab-I']
+    },
+    '1-2': {
+      theory: ['Mathematical Methods', 'Engineering Mechanics', 'Material Science & Metallurgy', 'Programming and Data Structures', 'Engineering Graphics and Computer Drafting', 'Environmental Science'],
+      labs: []
+    },
+    '2-1': {
+      theory: ['Transform Calculus', 'Kinematics of Machinery', 'Thermodynamics', 'Mechanics of Solids', 'Manufacturing Processes'],
+      labs: []
+    },
+    '2-2': {
+      theory: ['Design of Machine Elements', 'Dynamics of Machinery', 'Fluid Mechanics & Hydraulic Machinery', 'Metal Cutting and Machine Tools', 'Probability and Statistics', 'Indian Constitution'],
+      labs: []
+    },
+    '3-1': {
+      theory: ['Heat Transfer', 'Design of Transmission Elements', 'Applied Thermodynamics', 'Metrology and Mechanical Measurements'],
+      labs: ['English Language Communication Skills Lab-II']
+    },
+    '3-2': {
+      theory: ['Operations Research', 'Finite Element Method', 'Managerial Economics and Financial Analysis'],
+      labs: ['English Language Communication Skills Lab-III']
+    }
   }
   // Add other courses similarly...
 };
@@ -59,7 +158,8 @@ const SubjectList = () => {
   const year = query.get('year');
   const semester = query.get('semester');
   const key = `${year}-${semester}`;
-  const subjectList = subjects[courseId][key] || [];
+  const subjectsForSemester = subjects[courseId][key] || { theory: [], labs: [] };
+  const { theory, labs } = subjectsForSemester;
 
   return (
     <div>
@@ -78,10 +178,23 @@ const SubjectList = () => {
           <h2>Subjects for Year {year}, Semester {semester}</h2>
         </div>
         <div className="branches">
-          {subjectList.map((subject, index) => (
+          <div>
+          <h3>Theory Subjects</h3>
+          </div>
+          {theory.map((subject, index) => (
             <div key={index} className="card">
               <div className="card-content">
-                <h3>{subject}</h3>
+                <h4>{subject}</h4>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="branches">
+          <h3>Lab Subjects</h3>
+          {labs.map((lab, index) => (
+            <div key={index} className="card">
+              <div className="card-content">
+                <h4>{lab}</h4>
               </div>
             </div>
           ))}
